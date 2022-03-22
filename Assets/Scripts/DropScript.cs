96ac0569c83a51afc6232272522ab852f0857664
Keane <KeaneCarotenuto@gmail.com>
2022-03-22 16:57:48 +1300

    Creating Structure for tables and drop script

--- Assets/Scripts/DropScript.cs
+++ Assets/Scripts/DropScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -8,6 +10,25 @@ using UnityEditor;
 
 public class DropScript : MonoBehaviour
 {
+    [SerializeField] public List<DropTable> dropTables = new List<DropTable>();
+    [SerializeField] public bool combineTables = false;
+
+    [SerializeField] public Transform dropLocation = null;
+    [SerializeField] public Vector3 dropOffset = Vector3.zero;
+
+    //Drop options
+    [SerializeField] public bool dropOnDestroy = false;
+    [SerializeField] public bool allowMultipleDrops = false;
+    [SerializeField] public bool canDrop = true;
+    [SerializeField] public int maxDrops = 1;
+
+    //events
+    [SerializeField] public UnityEvent onSuccessfulDrop = new UnityEvent();
+    [SerializeField] public UnityEvent onFirstDrop = new UnityEvent();
+    [SerializeField] public UnityEvent onLastDrops = new UnityEvent();
+    [SerializeField] public UnityEvent onFailedDrop = new UnityEvent();
+
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/DropTable.cs
+++ Assets/Scripts/DropTable.cs
@@ -1,14 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
 //Drop table scriptable object + Editor script
+[Serializable]
 public class DropTable : ScriptableObject
 {
+    public enum Rarity
+    {
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary
+    }
+
+    //basic int range for easier editor customising
+    [Serializable]
+    public class IntRange {
+        [SerializeField] public int min = 0;
+        [SerializeField] public int max = 0;
+
+        public IntRange(int min = 0, int max = 0) {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+
+    //the objects that the table will drop, along with some options
+    [Serializable]
+    public class DropTableEntry
+    {
+        [SerializeField] public GameObject prefab = null;
+        [SerializeField] bool forced = false;
+        [SerializeField] public int weight = 1;
+        [SerializeField] public IntRange amountToDrop = new IntRange();
+        [SerializeField] public bool allowDuplicates = true;
+
+        public DropTableEntry(GameObject prefab = null, int weight = 0, bool forced = false, IntRange amountRange = null) {
+            this.prefab = prefab;
+            this.weight = weight;
+            this.forced = forced;
+            this.amountToDrop = amountRange;
+        }
+    }
+
+    [SerializeField] public Rarity rarity = Rarity.Common;
+    [SerializeField] public List<DropTableEntry> dropList = new List<DropTableEntry>();
+    [SerializeField] public IntRange numberOfDrops = new IntRange();
+    [SerializeField] public bool allowDuplicates = true;
+
+    [SerializeField] public int tableWeight = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +72,7 @@ public class DropTable : ScriptableObject
 
     #if UNITY_EDITOR
 
-    
+
 
     #endif
 }
